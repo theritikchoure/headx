ee51f796dedf7289495b1c2a8748b5f21f18bdb2
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-07-01 16:45:54 +0530

    First Release

--- src/Headx.jsx
+++ src/Headx.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 
-const Headx = () => {
-  const btn = () => {
-    console.log('hb');
+const Headx = (props) => {
+
+  const tags = (props) => {
+    const { title, meta } = props
+
+    if(title) {
+        document.title = props.title
+    }
+
+    if(meta.description) {
+        const metaRobots = document.createElement('meta');
+        metaRobots.name = 'description';
+        metaRobots.content = meta.description;
+        document.head.appendChild(metaRobots);
+    }
+    
+    if(meta.author) {
+        const metaRobots = document.createElement('meta');
+        metaRobots.name = 'author';
+        metaRobots.content = meta.author;
+        document.head.appendChild(metaRobots);
+    }
+    
+    if(meta.refresh) {
+        const metaRobots = document.createElement('meta');
+        metaRobots.httpEquiv = 'refresh';
+        metaRobots.content = meta.refresh;
+        document.head.appendChild(metaRobots);
+    }
+    
   }
 
   return (
-    <React.Fragment>{btn()}</React.Fragment>
+    <React.Fragment>{tags(props)}</React.Fragment>
   )
 }
 
