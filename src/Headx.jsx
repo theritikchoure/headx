e4559f17ccfdd2e9525e61da71950eddd815e6a3
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-07-01 17:13:33 +0530

    Update Headx.jsx

--- src/Headx.jsx
+++ src/Headx.jsx
@@ -3,31 +3,37 @@ import React from 'react'
 const Headx = (props) => {
 
   const tags = (props) => {
-    const { title, meta } = props
+    const { title, meta, httpEquiv, links } = props
 
     if(title) {
         document.title = props.title
     }
 
-    if(meta.description) {
-        const metaRobots = document.createElement('meta');
-        metaRobots.name = 'description';
-        metaRobots.content = meta.description;
-        document.head.appendChild(metaRobots);
+    if(meta) {
+        meta.map((meta) => {
+            const metaRobots = document.createElement('meta');
+            metaRobots.name = meta.name;
+            metaRobots.content = meta.content;
+            document.head.appendChild(metaRobots);
+        })
     }
     
-    if(meta.author) {
-        const metaRobots = document.createElement('meta');
-        metaRobots.name = 'author';
-        metaRobots.content = meta.author;
-        document.head.appendChild(metaRobots);
+    if(httpEquiv) {
+        httpEquiv.map((http_equiv) => {
+            const httpEquivTag = document.createElement('meta');
+            httpEquivTag.httpEquiv = http_equiv.name;
+            httpEquivTag.content = http_equiv.content;
+            document.head.appendChild(httpEquivTag);
+        })
     }
-    
-    if(meta.refresh) {
-        const metaRobots = document.createElement('meta');
-        metaRobots.httpEquiv = 'refresh';
-        metaRobots.content = meta.refresh;
-        document.head.appendChild(metaRobots);
+
+    if(links) {
+        links.map((link) => {
+            const linkTag = document.createElement('link');
+            linkTag.rel = link.name;
+            linkTag.href = link.content;
+            document.head.appendChild(linkTag);
+        })
     }
     
   }
